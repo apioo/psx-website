17d4a97e1d5ec903a0bd33afe89e86add82863ad
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-08-22 09:06:00 +0200

    update features

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -6,6 +6,7 @@ import {BiCodeBlock} from "react-icons/bi";
 import {VscDebugDisconnect} from "react-icons/vsc";
 import {DiDoctrine, DiSymfonyBadge} from "react-icons/di";
 import {SiOpenapiinitiative} from "react-icons/si";
+import CodeBlock from '@theme/CodeBlock';
 
 const FeatureList = [
     {
@@ -22,7 +23,7 @@ const FeatureList = [
         Svg: BiCodeBlock,
         description: (
             <>
-                Client SDK generator which supports <b>TypeScript</b>, <b>PHP</b>, <b>Java</b>, <b>Go</b>
+                Client SDK generator
             </>
         ),
     },
@@ -31,12 +32,12 @@ const FeatureList = [
         Svg: SiOpenapiinitiative,
         description: (
             <>
-                <a href="https://www.openapis.org/">OpenAPI</a> generation
+                <a href="https://www.openapis.org/">OpenAPI</a> generator
             </>
         ),
     },
     {
-        title: 'TypeSchema',
+        title: 'Model',
         Svg: VscDebugDisconnect,
         description: (
             <>
@@ -86,6 +87,82 @@ export default function HomepageFeatures() {
                     {FeatureList.map((props, idx) => (
                         <Feature key={idx} {...props} />
                     ))}
+                </div>
+                <div className="row">
+                    <div className="col col--12 text--center">
+                        <hr></hr>
+                        <h1 className="hero__title">Controller</h1>
+                        <p className="hero__subtitle">PSX allows you to define complete type-safe controllers which map
+                            every value from the HTTP request to an argument.
+                            This idea is inspired by great frameworks like <a href="https://spring.io/">Spring</a> or <a
+                            href="https://nestjs.com/">NestJS</a>.</p>
+                    </div>
+                    <div className="col col--12">
+                        <CodeBlock language="php" title="src/Controller/Population.php" showLineNumbers>{`class Population extends ControllerAbstract
+{
+    #[Get]
+    #[Path('/population')]
+    public function getAll(#[Query] ?int $startIndex = null, #[Query] ?int $count = null): Model\\PopulationCollection
+    {
+        // @TODO return population entries
+    }
+
+    #[Post]
+    #[Path('/population')]
+    #[StatusCode(201)]
+    public function create(#[Body] Model\\Population $payload): Model\\Message
+    {
+        // @TODO create population entry
+    }
+}
+`}</CodeBlock>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col col--12 text--center">
+                        <hr></hr>
+                        <h1 className="hero__title">Dependency Injection</h1>
+                        <p className="hero__subtitle">A key concept in PSX is dependency injection. Internally we use
+                            the Symfony <a
+                                href="https://symfony.com/doc/current/components/dependency_injection.html">DependencyInjection</a> Component
+                            to automatically inject all dependencies into your controller so that you can build a clean
+                            architecture.</p>
+                    </div>
+                    <div className="col col--12">
+                        <CodeBlock language="php" title="src/Controller/Population.php" showLineNumbers>{`class Population extends ControllerAbstract
+{
+    public function __construct(private MyService $myService)
+    {
+    }
+
+    #[Post]
+    #[Path('/population')]
+    #[StatusCode(201)]
+    public function create(#[Body] Model\\Population $payload): Model\\Message
+    {
+        $id = $this->myService->create($payload);
+
+        $message = new Model\\Message();
+        $message->setSuccess(true);
+        $message->setMessage('Population record successfully created');
+        $message->setId($id);
+        return $message;
+    }
+}
+`}</CodeBlock>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col col--12 text--center">
+                        <hr></hr>
+                        <h1 className="hero__title">Client SDK</h1>
+                        <p className="hero__subtitle">One of the innovative features of PSX is, that you can automatically
+                        generate a client SDK for your API. Through a simple command you can generate
+                        a complete type-safe client SDK to communicate with your API.</p>
+                    </div>
+                    <div className="col col--12">
+                        <CodeBlock language="text" title="Console" showLineNumbers>{`php bin/psx generate:sdk`}</CodeBlock>
+                    </div>
                 </div>
             </div>
         </section>
